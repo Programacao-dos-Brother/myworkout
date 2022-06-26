56e533dbd26f77aaf5aeeaa7a19207ed59f177c0
Leonardo Portes <l.portes@portaltelemedicina.com.br>
2022-06-26 00:50:51 -0300

    Implement authentication base component

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
-
+import AppRoutes from "./routes";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      
+        <AppRoutes />
       </BrowserRouter>
     </div>
   );
--- web/src/routes.js
+++ web/src/routes.js
@@ -1,9 +1,17 @@
 import { Routes } from "react-router-dom";
+import LoginPage from "./pages/Login/LoginPage";
 
 const AppRoutes = () => {
+    // Implement auth verification here
+    const isUserAuthenticated = false;
+    if (!isUserAuthenticated) {
+        return <LoginPage />;
+    }
+
     return (
         <Routes>
-            
         </Routes>
     );
 }
+
+export default AppRoutes;
