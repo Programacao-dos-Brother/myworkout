69631a3de1fe9c800dd1ddff2338444f035d3ae3
Pedro de Barros <pedro.barros@globalbot.com.br>
2022-06-26 01:11:52 -0300

    goals routes

--- api/model/Goals.js
+++ api/model/Goals.js
@@ -2,12 +2,24 @@ const mongoose = require('mongoose')
 
 const GoalsSchema = new mongoose.Schema({
     name: String
-}, {
-    collation: 'goal'
 })
 
 const Goals = mongoose.model('Goals', GoalsSchema)
 
 module.exports.createGoal = async (payload) => {
     return Goals.create(payload)
+}
+
+module.exports.readGoal = async () => {
+    console.log('tchau')
+    const goals = await Goals.find()
+    return goals
+}
+
+module.exports.deleteGoal = async (id) => {
+    return await Goals.deleteOne({_id: id})
+}
+
+module.exports.updateGoal = async (id, name) => {
+    return await Goals.updateOne({_id: id}, {name: name})
 }
--- api/routes/goals.js
+++ api/routes/goals.js
@@ -10,4 +10,33 @@ routes.post('/create', async (req, res) => {
     }
 })
 
+routes.get('/read', async (req, res) => {
+    try {
+        console.log('oi')
+        res.send(await Goals.readGoal())
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+routes.delete('/delete/:id', async (req, res) => {
+    try {
+        res.send(await Goals.deleteGoal(req.params.id))
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+routes.put('/update/:id', async (req, res) => {
+    try {
+        if (req.body.name && req.body.name !== '') {
+            res.send(await Goals.updateGoal(req.params.id, req.body.name))
+        } else {
+            res.send({error: 'Missing name'})
+        }
+    } catch (e) {
+        console.log(e)
+    }
+})
+
 module.exports = routes
