891269315376fcc4777479539a6b4a657d0264fc
Elivelton Fernando <eliveltonfoliveira@gmail.com>
2022-07-20 20:10:38 -0300

    adding category to equipment schemma.

--- api/schema/equipment.js
+++ api/schema/equipment.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose')
 const Equipment = new mongoose.Schema(
     {
         name: String,
+        category: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'ExerciseCategory'
+        },
         tagNumber: Number,
         numberEquipment: Number,
         imageEquipment: String
